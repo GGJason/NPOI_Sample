95ea607f0ca8ecc682f61234c9d40d04c7644548
吳軒竹 <jasonwu@webim.com.tw>
2021-04-13 21:12:34 +0800

    Update .gitignore

---
+++ TestCreateWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using NPOI.XWPF.UserModel;
+
+namespace TestCreateWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var templateFile = "./../../../Doc1.docx";
+            var destinationFile = "output.docx";
+            using (FileStream file = new FileStream(templateFile, FileMode.Open, FileAccess.Read))
+            {
+                XWPFDocument wordDoc = new XWPFDocument(file);
+
+                var fs = new FileStream(destinationFile, FileMode.OpenOrCreate, FileAccess.Write);
+
+                wordDoc.Write(fs);
+                fs.Close();
+
+                wordDoc.Close();
+            }
+        }
+    }
+}
